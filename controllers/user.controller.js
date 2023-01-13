cb36bc154b82935af5c8a9731645487dc8c2f892
Olexandr Kovalenko <22bendera.s@gmail.com>
2023-01-13 14:48:11 +0200

    User CRUD

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,8 +1,8 @@
-const {User} = require('../models');
+const { User } = require('../models');
 
 module.exports.createUser = async (req, res, next) => {
-    try {      
-        const {body} = req;
+    try {
+        const { body } = req;
         const result = await User.create(body);
         res.status(201).send(result);
     } catch (error) {
@@ -12,8 +12,8 @@ module.exports.createUser = async (req, res, next) => {
 
 module.exports.getOneUser = async (req, res, next) => {
     try {
-        const {params: {userId}} = req;
-        const result = await User.findByPk(userId).then(data=>data);
+        const { params: { userId } } = req;
+        const result = await User.findByPk(userId);
         res.status(200).send(result);
     } catch (error) {
         next(error)
@@ -31,8 +31,8 @@ module.exports.getAllUser = async (req, res, next) => {
 
 module.exports.updateUser = async (req, res, next) => {
     try {
-        const {params: {userId}, body} = req;
-        const [id, [result]] = await User.update(body, {returning: true, where: {id: userId}}).then(data=>data);
+        const { params: { userId }, body } = req;
+        const [rowCount, [result]] = await User.update(body, { returning: true, where: { id: userId } });
         res.status(200).send(result);
     } catch (error) {
         next(error)
@@ -41,9 +41,35 @@ module.exports.updateUser = async (req, res, next) => {
 
 module.exports.deleteUser = async (req, res, next) => {
     try {
-        const {params: {userId}} = req;
-        const result = await User.destroy({where: {id: userId}}).then(data=>data);
-        res.status(200).send();
+        const { params: { userId } } = req;
+        // const user = await User.findByPk(userId);
+        const result = await User.destroy({
+            where: { id: userId }
+        });
+        if (result) {
+            res.status(200).send({ data: result });
+        } else {
+            res.status(404).send('Not founded');
+        }
+        res.status(200).send(user);
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+
+
+
+// Видалення сутності
+module.exports.deleteInstanse = async (req, res, next) => {
+    try {
+        const { params: { userId } } = req;
+        const user = await User.findByPk(userId);
+        if (user) {
+            const result = await user.destroy();
+        }
+        res.status(200).send(result);
     } catch (error) {
         next(error)
     }
